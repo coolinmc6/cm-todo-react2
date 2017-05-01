b830531521a3ed85716d7e4d60fb7425ea84063a
Colin McNamara <coolinmc6@gmail.com>
2017-05-01 09:38:56 -0400

    Add Redux

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { Provider } from 'react-redux';
+import {createStore, applyMiddleware } from 'redux';
+import reducers from './reducers';
+
+const createStoreWithMiddleware = applyMiddleware()(createStore);
+
+
+import App from './components/App';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+	<Provider store={createStoreWithMiddleware(reducers)}>
+  		<App />
+	</Provider>,
   document.getElementById('root')
 );
